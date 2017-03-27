92506bb4d070d08ab2579a1cb8c8123e8cd1c243
Evan Chang <chin.yeng@gmail.com>
2017-03-27 15:57:06 +0800

    Create 5_PassingCars.py

---
+++ 5_PassingCars.py
@@ -0,0 +1,26 @@
+def solution(A):
+    N = len(A)
+    if N < 1 or N > 100000:
+        return -1
+
+    west = 0
+    passing = 0
+
+    for i in xrange(N):
+        if A[i] < 0 or A[i] > 1:
+            return -1
+
+        if A[i] == 0:
+            west += 1
+        else:
+            passing += west
+            if passing > 1000000000:
+                return -1
+
+        #print("west = {}".format(west))
+        #print("passing = {}".format(passing))
+        #print("=======")
+
+    return passing
+
+print solution([0,1,0,1,1])
