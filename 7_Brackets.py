a0062cd67616cbd5ec9f7e086bebe7b9027dcf07
Evan Chang <chin.yeng@gmail.com>
2017-04-06 17:17:45 +0800

    Create 7_Brackets.py

---
+++ 7_Brackets.py
@@ -0,0 +1,30 @@
+def solution(S):
+    n = len(S)
+    if n < 0 or n > 200000:
+        return 0
+
+    if len(S.strip()) == 0: # S is empty
+        return 1
+
+    match_bracket = {')':'(', ']':'[', '}':'{'}
+    to_push_bracket = ['(', '[', '{']
+
+    bracket_stack = []
+    for bracket in S:
+        if bracket not in ['(', ')', '[', ']', '{', '}']:
+            return 0
+
+        if bracket in to_push_bracket:
+            bracket_stack.append(bracket)
+        else:
+            if len(bracket_stack) == 0:
+                return 0
+            if match_bracket[bracket] != bracket_stack.pop():
+                return 0
+
+    if len(bracket_stack) == 0:
+        return 1
+    else:
+        return 0
+
+print solution("([)")
